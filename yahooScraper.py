a01eb9e972c589f001b948b73cb0431c28cf1e9f
Spwall319 <143623456+Spwall319@users.noreply.github.com>
2024-09-24 23:11:22 -0400

    Update yahooScraper.py
    Added the get_stock_info function to get all statistics of a stock

--- yahooScraper.py
+++ yahooScraper.py
@@ -12,9 +12,23 @@ def web_content_div(web_content, class_path):
     except IndexError:
         texts = []
     return texts
+
+def get_stock_info(web_content, class_path):
+    info = {}
+    web_content_div = web_content.find_all('div', attrs = {'class':class_path, 'data-testid':'quote-statistics'})
+    try:
+        lis = web_content_div[0].find_all('li')
+        for li in lis:
+            spans = li.find_all('span')
+            info[spans[0].get_text()] = spans[1].get_text()
+    except IndexError:
+        info = {}
+    return info
+
 def real_time_prices(stock_code):
     url = 'https://finance.yahoo.com/quote/' + stock_code + '/'
 
+    info = {}
     try:
         r = requests.get(url)
         web_content = BeautifulSoup(r.content, 'html.parser')
@@ -29,13 +43,27 @@ def real_time_prices(stock_code):
                 after['price'] = texts[4]
                 after['change'] = texts[5]
                 after['perChange'] = texts[6][1:-1]
-                return day, after
+            else:
+                after = {}
         else:
             day = {}
+            after = {}
+
+        info = get_stock_info(web_content, 'container yf-tx3nkj')
+        print(info)
+
+        pattern = web_content_div(web_content, '')
+        try:
+            latest_pattern = pattern[0]
+        except IndexError:
+            latest_pattern = []
     except ConnectionError:
         day = {}
-    return day, {}
+        after = {}
+        info = {}
+    return day, after, info
 
+"""
 Stock = ['NVDA', 'TIGR', 'MTTR']
 for s in Stock:
     print("Ticker: " + s)
@@ -47,3 +75,5 @@ for s in Stock:
         for key, val in lst[1].items():
             print(key + ": " + val)
     print('\n')
+lst = real_time_prices('NVDA')
+"""
